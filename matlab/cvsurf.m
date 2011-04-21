3e4f630b6aa4e01ac2d507c75e36472f4c10f3cd
simbaforrest <simbaforrest@localhost>
2011-04-20 20:34:28 -0400

    modified cvsurf

--- matlab/cvsurf.m
+++ matlab/cvsurf.m
@@ -4,5 +4,8 @@
 % OUTPUT
 %   [keys <cell, Kx1>]: optional, detected keypoints for each image, <Nx3>
 %   [matches <cell, KxK>]: optional, matched idx, <Mx2>
+%       matches{i,j}(k,:)=[m,n] means image i is training image, image j is
+%       query image, and keys{i}(m,:) is matched with keys{j}(n,:)
+%       normally, n will be equal to k, since n stores the trainIdx
 % AUTHOR
 %   Chen Feng <simbaforrest@gmail.com>
