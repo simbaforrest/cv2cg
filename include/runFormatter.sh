3256e57dd4dc88961d057fc45fe43d9d384a5098
simbaforrest <simbaforrest@localhost>
2011-05-03 11:55:16 -0400

    add 	some scripts for test include files 	some matlab functions

---
+++ include/runFormatter.sh
@@ -0,0 +1,2 @@
+#sudo apt-get install astyle
+astyle -A10 -t -k3 *.h
---
+++ matlab/RTfromKH.m
@@ -0,0 +1,13 @@
+function [R,T]=RTfromKH(K,H)
+% compute plane pose (R and T) from K and Homography
+% by Chen Feng <simbaforrest@gmail.com>
+
+% H = const * K[r1,r2,T]
+A = inv(K) * H;
+A = A/norm(A(:,1)); % get rid of const by apply ||r1||=1
+r1 = A(:,1);
+r2 = A(:,2);
+T  = A(:,3);
+r3 = cross(r1,r2);
+R = [r1,r2,r3];
+end
