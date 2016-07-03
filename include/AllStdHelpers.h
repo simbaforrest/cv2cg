02b101441a117f8e4ff6319d12445b8d7c9563df
cfeng <cfeng@merl.com>
2016-07-02 21:48:09 -0400

    incorporate lch's addition of TimeHelper.hpp

--- include/AllStdHelpers.h
+++ include/AllStdHelpers.h
@@ -3,6 +3,7 @@
    a set of helper functions for general C/C++ */
 
 //helper include
+#define OPENCV_FOUND //for TimeHelper.hpp, use opencv's gitTickCount function
 #include "lch.hpp"
 
 #include "ClusterHelper.h"
